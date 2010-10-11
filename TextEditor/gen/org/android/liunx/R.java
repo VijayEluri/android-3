e31f8daa038d6b42cbb7a6ba34b9d112a360e2ed
Liunx <liunx163@163.com>
2010-10-11 23:44:29 +0000

    Add a menu item and dialog .

--- TextEditor/gen/org/android/liunx/R.java
+++ TextEditor/gen/org/android/liunx/R.java
@@ -18,6 +18,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int delete=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int open=0x7f040002;
+        public static final int save=0x7f040003;
     }
 }
--- TextEditor/src/org/android/liunx/texteditor.java
+++ TextEditor/src/org/android/liunx/texteditor.java
@@ -1,13 +1,86 @@
 package org.android.liunx;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class texteditor extends Activity {
+	// the menus
+	private static final int MENU_OPEN = Menu.FIRST;
+	private static final int MENU_SAVE = Menu.FIRST + 1;
+	private static final int MENU_DELETE = Menu.FIRST + 2;
+	// the dialogs
+	private static final int DIALOG_OPEN = 1;
+	private static final int DIALOG_SAVE = 2;
+	private static final int DIALOG_DELETE = 4;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    /**
+     * We can do open/save/delete operations with menu 
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	menu.add(0, MENU_OPEN, 0, R.string.open);
+    	menu.add(0, MENU_SAVE, 0, R.string.save);
+    	menu.add(0, MENU_DELETE, 0, R.string.delete);
+    	return true;
+    }
+    
+    /**
+     * process the menu press events
+     */
+    @Override 
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// get the id which menu is selected
+    	final int id = item.getItemId();
+    	boolean retval = false;
+    	switch (id) {
+    	case MENU_OPEN:
+    		showDialog(DIALOG_OPEN);
+    		retval = true;
+    		break;
+    	case MENU_SAVE:
+    		showDialog(DIALOG_SAVE);
+    		retval = true;
+    		break;
+    	case MENU_DELETE:
+    		showDialog(DIALOG_DELETE);
+    		retval =true;
+    		break;
+    		default:
+    			break;
+    	}
+    	return retval;
+    }
+    /**
+     * when howDialog, onDialogCreate will be called 
+     */
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	// the switch sentence
+    	switch (id) {
+    	case DIALOG_OPEN:
+    		Toast.makeText(this, "Dialog open", Toast.LENGTH_LONG).show();
+    		break;
+    	case DIALOG_SAVE:
+    		Toast.makeText(this, "Dialog save", Toast.LENGTH_LONG).show();
+    		break;
+    	case DIALOG_DELETE:
+    		Toast.makeText(this, "Dialog delete", Toast.LENGTH_LONG).show();
+    		break;
+    	}
+    	// we can not return null
+    	return null;
+    	
     }
 }
